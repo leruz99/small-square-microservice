5dee2c2f4f57e354c85c6980066ad0dac6df8a49
luis.ruz <luis.ruz@ceiba.com.co>
2023-05-24 21:52:07 -0500

    iniciando auth

--- src/main/java/com/pragma/powerup/smallsquaremicroservice/configuration/security/jwt/JwtEntryPoint.java
+++ src/main/java/com/pragma/powerup/smallsquaremicroservice/configuration/security/jwt/JwtEntryPoint.java
@@ -1,4 +1,28 @@
 package com.pragma.powerup.smallsquaremicroservice.configuration.security.jwt;
 
-public class JwtEntryPoint implements AuthenticationEntryPoint{
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+import java.io.IOException;
+
+@Configuration
+public class JwtEntryPoint implements AuthenticationEntryPoint {
+    @Autowired
+    @Qualifier("handlerExceptionResolver")
+    private HandlerExceptionResolver resolver;
+
+
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        resolver.resolveException(request,response, null, exception);
+
+    }
 }
